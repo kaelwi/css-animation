32201e5792af6989e4df0dff2801a1ebb463ee1c
kaelwi <karoline.elisabeth.wild@seznam.cz>
2022-10-19 23:44:38 +0200

    Animation examples

---
+++ bsp/animation/script.js
@@ -0,0 +1,12 @@
+const shake = document.getElementById('shake');
+const exit = document.getElementById('exit');
+const container = document.getElementById('container');
+const red = document.getElementById('red');
+const offset = document.getElementById('offset');
+
+/* Tipp: 
+    - events: click, mouseover, mouseout
+    - currentTarget vs. target
+    - setTimeout um div nach Ende der Animation zu entfernen
+        (https://developer.mozilla.org/en-US/docs/Web/API/setTimeout)
+*/
