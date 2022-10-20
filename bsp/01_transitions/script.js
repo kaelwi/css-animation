b69834b28ea0660fa4d3be637200a85c7a672d28
kaelwi <karoline.elisabeth.wild@seznam.cz>
2022-10-20 08:50:26 +0200

    Ternary operator <-> if/else

--- bsp/01_transitions/script.js
+++ bsp/01_transitions/script.js
@@ -3,4 +3,11 @@ const div = document.getElementById('onevent');
 div.addEventListener('click', () => {
     let bg = window.getComputedStyle(div).getPropertyValue('background-color');
     div.style.backgroundColor = bg == 'rgb(255, 0, 0)' ? 'black' : 'red';
+    /*
+        if (bg == 'rgb(255, 0, 0)) {
+            div.style.backgroundColor = 'black';
+        } else {
+            div.style.backgroundColor = 'red';
+        }
+    */
 })
