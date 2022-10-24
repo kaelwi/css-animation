d1f57baf1f2df11cef519e0e4c08810865b016b1
kaelwi <karoline.elisabeth.wild@seznam.cz>
2022-10-24 18:34:50 +0200

    animation solution

--- bsp/02_animation/script.js
+++ bsp/02_animation/script.js
@@ -4,9 +4,44 @@ const container = document.getElementById('container');
 const red = document.getElementById('red');
 const offset = document.getElementById('offset');
 
-/* Tipp: 
-    - events: click, mouseover, mouseout
-    - currentTarget vs. target
-    - setTimeout um div nach Ende der Animation zu entfernen
-        (https://developer.mozilla.org/en-US/docs/Web/API/setTimeout)
-*/
+shake.addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('shake');
+});
+
+exit.addEventListener('click', (e) => {
+    e.currentTarget.classList.add('exit');
+});
+
+container.addEventListener('mouseover', scale);
+container.addEventListener('mouseout', scale);
+container.addEventListener('click', remove);
+
+function scale(e) {
+    if (e.target != e.currentTarget) {
+        e.target.classList.toggle('scale');
+    }
+}
+
+function remove(e) {
+    currentTarget = e.currentTarget;
+    if (e.target != e.currentTarget) {
+        if (e.target != red) {
+            e.target.classList.add('remove');
+            setTimeout(function() {
+                currentTarget.removeChild(e.target);
+            }, 1000);
+        } else {
+            e.target.classList.add('shake');
+            setTimeout(function() {
+                e.target.classList.remove('shake');
+            }, 1000);
+        }
+    }
+}
+
+offset.addEventListener('click', (e) => {
+    e.target.classList.add('path');
+    setTimeout(function() {
+        e.target.classList.remove('path');
+    }, 6000);
+})
